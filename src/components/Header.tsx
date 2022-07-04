cfb177867d2fb739a03f8870efa113563d717617
rtakaoka <31188845+rtakaoka@users.noreply.github.com>
2022-07-04 18:14:14 -0300

    Full App

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import LogoPersuasion from "./LogoPersuasion";
+
+export function Header(){
+    return (
+    <header className="w-full py-5 flex items-center justify-center bg-gray-700 border-b border-gray-600">
+        <LogoPersuasion />
+    </header>
+    )
+}
