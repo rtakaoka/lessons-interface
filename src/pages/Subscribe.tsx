1de3011577f5b7e047a277c9971e8c44507ffe89
rtakaoka <31188845+rtakaoka@users.noreply.github.com>
2022-07-05 13:41:50 -0300

    Logo updated in Subscribe Page

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -1,5 +1,6 @@
 import { useState, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
+import Logo from "../components/Logo";
 import LogoPersuasion from "../components/LogoPersuasion";
 import { useCreateSubscriberMutation } from "../graphql/generated";
 
@@ -30,7 +31,7 @@ export function Subscribe() {
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
       <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
         <div className="max-w-[640px]">
-          <LogoPersuasion />
+          <Logo />
           <h1 className="mt-8 text-[2.5rem] leading-tight">
             Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React</strong>
           </h1>
